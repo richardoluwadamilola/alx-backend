c4becbbd36cfbadddb5ce4ec7a9df6f919a91550
richardoluwadamilola <oluwatobyrichard@gmail.com>
2023-05-16 09:57:54 +0000

    Lifo caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Last-In First-Out caching module"""
+from collections import OrderedDict
+
+from base-caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Represents an object that allows storing and retrieving items from
+    a dictionary using the LIFO method"""
+    def __init__(self):
+        """Initializes the cache"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache"""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", laast_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """Retrieves an item by key"""
+        return self.cache_data.get(key, None)
