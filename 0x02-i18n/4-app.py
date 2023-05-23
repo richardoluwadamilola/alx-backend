59b0be8e1f0c1745e8382984cca866301582f234
richardoluwadamilola <oluwatobyrichard@gmail.com>
2023-05-23 10:39:51 +0000

    fix

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-"""A Basic flask app with internationalized support."""
+"""A Basic Flask app with internationalization support.
+"""
 from flask_babel import Babel
 from flask import Flask, render_template, request
 
 
 class Config:
-    """Represents a Flask Babel configuration"""
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTF"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
@@ -17,9 +19,10 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@babel.localselector
+@babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page."""
+    """Retrieves the locale for a web page.
+    """
     queries = request.query_string.decode('utf-8').split('&')
     query_table = dict(map(
         lambda x: (x if '=' in x else '{}='.format(x)).split('='),
@@ -28,12 +31,13 @@ def get_locale() -> str:
     if 'locale' in query_table:
         if query_table['locale'] in app.config["LANGUAGES"]:
             return query_table['locale']
-        return request.accept_languages.best_match(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
 def get_index() -> str:
-    """The home/index page"""
+    """The home/index page.
+    """
     return render_template('4-index.html')
 
 
